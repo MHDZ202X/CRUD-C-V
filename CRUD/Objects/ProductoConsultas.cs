331e08cc7aee9ff27d2cceb488debd7d8bd3ce49
MHDZ202X <alonso.hernandez97@gmail.com>
2022-12-11 18:11:38 -0600

    first commit

---
+++ CRUD/Objects/ProductoConsultas.cs
@@ -0,0 +1,108 @@
+﻿using CRUD.BD;
+using CRUD.Modelo;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD.Objects
+{
+    internal class ProductoConsultas
+    { 
+        private ConexionMySql mConexion;
+        private List<Producto> mProductos;
+
+        public ProductoConsultas()
+        {
+            mConexion = new ConexionMySql();
+            mProductos = new List<Producto>();
+        }
+
+        public bool agregarProducto(Producto mProducto)
+        {
+            string INSERT = "INSERT INTO producto (nombre, descripcion, precio)" + " values (@nombre, @descripcion, @precio);";
+
+            MySqlCommand mCommand = new MySqlCommand(INSERT, mConexion.getConexion());
+            mCommand.Parameters.Add(new MySqlParameter("@nombre", mProducto.nombre));
+            mCommand.Parameters.Add(new MySqlParameter("@descripcion", mProducto.descripcion));
+            mCommand.Parameters.Add(new MySqlParameter("@precio", mProducto.precio));
+            //mCommand.Parameters.Add(new MySqlParameter("@categoria", mProducto.categoria));
+
+            return mCommand.ExecuteNonQuery() > 0;
+        }
+
+        public bool modificarProducto(Producto mProducto)
+        {
+            string UPDATE = " UPDATE producto " +
+                "SET nombre = @nombre, " +
+                "descripcion = @descripcion, " +
+                "precio = @precio, " +
+                //"categoria = @categoria " +
+                "WHERE idProducto = @id";
+
+            MySqlCommand mCommand = new MySqlCommand(UPDATE, mConexion.getConexion());
+            mCommand.Parameters.Add(new MySqlParameter("@nombre", mProducto.nombre));
+            mCommand.Parameters.Add(new MySqlParameter("@descripcion", mProducto.descripcion));
+            mCommand.Parameters.Add(new MySqlParameter("@precio", mProducto.precio));
+            //mCommand.Parameters.Add(new MySqlParameter("@categoria", mProducto.categoria));
+
+            return mCommand.ExecuteNonQuery() > 0;
+        }
+
+        public bool eliminarProducto(Producto mProducto)
+        {
+            string DELETE = " DELETE FROM producto WHERE idProducto=@id";
+            MySqlCommand mCommand = new MySqlCommand(DELETE, mConexion.getConexion());
+            mCommand.Parameters.Add(new MySqlParameter("@id", mProducto.id));
+            return mCommand.ExecuteNonQuery() > 0;
+        }
+
+        public List<Producto> consultarProductos(string filtro)
+        {
+            string CONSULTA = "SELECT * FROM producto";
+
+            MySqlDataReader mReader = null;
+            Producto mProducto;
+            try
+            {
+                if (filtro != "")
+                {
+                    CONSULTA += " WHERE " +
+                        "id LIKE '%" + filtro + "%' OR " +
+                        "nombre LIKE '%" + filtro + "%' OR " +
+                        "precio LIKE '%" + filtro + "%' OR " +
+                        "descripcion LIKE '%" + filtro + "%'OR;";
+                }
+
+                MySqlCommand mCommand = new MySqlCommand(CONSULTA);
+                mCommand.Connection = mConexion.getConexion();
+                mReader = mCommand.ExecuteReader();
+
+                while (mReader.Read())
+                {
+                    mProducto = new Producto();
+                    mProducto.id = mReader.GetInt16("idProducto");
+                    mProducto.nombre = mReader.GetString("nombre");
+                    mProducto.precio = mReader.GetInt16("precio");
+                    mProducto.descripcion = mReader.GetString("descripcion");
+                    //mProducto.categoria = (byte[]) mReader.GetValue(4);
+                    mProductos.Add(mProducto);
+                }
+                mReader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mConexion.closeConexion();
+            }
+
+            return mProductos;
+        }
+    }
+}
---
+++ CRUD/Visual/FRMProductos.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace CRUD
+{
+    partial class FRMProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMProductos));
+            this.pnlBackground = new System.Windows.Forms.Panel();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblFolio = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblImagen = new System.Windows.Forms.Label();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pnlBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlBackground.Controls.Add(this.txtCategoria);
+            this.pnlBackground.Controls.Add(this.btnLimpiar);
+            this.pnlBackground.Controls.Add(this.btnSalir);
+            this.pnlBackground.Controls.Add(this.btnEliminar);
+            this.pnlBackground.Controls.Add(this.btnModificar);
+            this.pnlBackground.Controls.Add(this.txtId);
+            this.pnlBackground.Controls.Add(this.lblFolio);
+            this.pnlBackground.Controls.Add(this.btnAgregar);
+            this.pnlBackground.Controls.Add(this.lblImagen);
+            this.pnlBackground.Controls.Add(this.dgvProductos);
+            this.pnlBackground.Controls.Add(this.lblCantidad);
+            this.pnlBackground.Controls.Add(this.lblPrecio);
+            this.pnlBackground.Controls.Add(this.lblNombre);
+            this.pnlBackground.Controls.Add(this.txtDescripcion);
+            this.pnlBackground.Controls.Add(this.txtPrecio);
+            this.pnlBackground.Controls.Add(this.txtNombre);
+            this.pnlBackground.Location = new System.Drawing.Point(-4, -2);
+            this.pnlBackground.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(767, 563);
+            this.pnlBackground.TabIndex = 0;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(566, 95);
+            this.txtCategoria.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(165, 20);
+            this.txtCategoria.TabIndex = 13;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(247, 515);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(58, 34);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar  ";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(309, 515);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(43, 34);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "Salir  ";
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(183, 515);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(60, 34);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar  ";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnModificar.Location = new System.Drawing.Point(107, 515);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(67, 34);
+            this.btnModificar.TabIndex = 9;
+            this.btnModificar.Text = "Modificar  ";
+            this.btnModificar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnModificar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(100, 70);
+            this.txtId.Margin = new System.Windows.Forms.Padding(2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(165, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // lblFolio
+            // 
+            this.lblFolio.AutoSize = true;
+            this.lblFolio.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFolio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFolio.Location = new System.Drawing.Point(16, 68);
+            this.lblFolio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFolio.Name = "lblFolio";
+            this.lblFolio.Size = new System.Drawing.Size(21, 18);
+            this.lblFolio.TabIndex = 10;
+            this.lblFolio.Text = "id";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAgregar.Location = new System.Drawing.Point(20, 515);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(83, 34);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lblImagen
+            // 
+            this.lblImagen.AutoSize = true;
+            this.lblImagen.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagen.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblImagen.Location = new System.Drawing.Point(592, 56);
+            this.lblImagen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblImagen.Name = "lblImagen";
+            this.lblImagen.Size = new System.Drawing.Size(73, 18);
+            this.lblImagen.TabIndex = 7;
+            this.lblImagen.Text = "categoria";
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
+            this.dgvProductos.AllowUserToOrderColumns = true;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Nombre,
+            this.Precio,
+            this.descripcion});
+            this.dgvProductos.Location = new System.Drawing.Point(20, 274);
+            this.dgvProductos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(456, 234);
+            this.dgvProductos.TabIndex = 7;
+            this.dgvProductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProductos_CellClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 50;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 150;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "descripcion";
+            this.descripcion.MinimumWidth = 6;
+            this.descripcion.Name = "descripcion";
+            this.descripcion.ReadOnly = true;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCantidad.Location = new System.Drawing.Point(16, 176);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(87, 18);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "descripcion";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPrecio.Location = new System.Drawing.Point(16, 141);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(55, 18);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNombre.Location = new System.Drawing.Point(16, 106);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(70, 18);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(100, 176);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(165, 20);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(100, 142);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(165, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(100, 108);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(165, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(541, 626);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 42);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(372, 626);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 42);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Eliminar  ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FRMProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 557);
+            this.Controls.Add(this.pnlBackground);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FRMProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Productos";
+            this.pnlBackground.ResumeLayout(false);
+            this.pnlBackground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBackground;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblFolio;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+    }
+}
+
